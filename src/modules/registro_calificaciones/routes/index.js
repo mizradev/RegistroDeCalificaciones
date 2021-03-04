1efdb6125a3773608c489f06f2af85d7b448123e
soymizra <jorgemizrain@gmail.com>
2021-03-04 01:18:33 -0600

    Rutas y controladores agregados

--- src/modules/registro_calificaciones/routes/index.js
+++ src/modules/registro_calificaciones/routes/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const mysqlConnection = require('../../../config/db');
 //controllers
 const CalificacionesCtrl = require('../controllers/CalificacionesCtrl');
 
 //const { check } = require('express-validator');
 
-router.get('/', (req, res)=>{
-    res.send('Hola mundo');
-});
+router.get('/', CalificacionesCtrl.obtenerAlumnosPorIdDocente);
+router.get('/:id', CalificacionesCtrl.obtenerNotasPorIdAlumno);
+router.post('/actividades', CalificacionesCtrl.registrarActividad);
 
 
 module.exports = router;
