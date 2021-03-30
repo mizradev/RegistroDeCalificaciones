9616f3efb52373168273f4be177d7bbd0ba07c8f
JeralPineda <jeral.cris@gmail.com>
2021-03-30 13:07:59 -0600

    Cambiando la ruta de inicio a home y agregando index como principal

--- src/frontend/controllers/auth/AuthCtrl.js
+++ src/frontend/controllers/auth/AuthCtrl.js
@@ -1,41 +1,50 @@
 exports.pageAuth = (req, res) => {
-    res.render('auth/login', {
-        nombrePagina: 'login Sistema',
-        pageAuthLogin: true,
-        layout: false
-    })
-}
+   res.render('auth/login', {
+      nombrePagina: 'login Sistema',
+      pageAuthLogin: true,
+      layout: false,
+   });
+};
+
+exports.home = (req, res) => {
+   if (true)
+      return res.render('blank', {
+         nombrePagina: 'Home',
+      });
+};
 
 exports.pantalla_inicio = (req, res) => {
-    if(true)
-        return res.render('blank', {
-            nombrePagina: 'Inicio'
-        });
-    
-    return res.render('auth/index', {
-        nombrePagina: 'Registro de Calificaciones',
-        pageAuthIndex: true,
-        layout: 'auth-layout'
-    })
-}
+   if (true)
+      return res.render('auth/index', {
+         nombrePagina: 'Inicio',
+         pageAuthLogin: true,
+         layout: false,
+      });
+
+   //    return res.render('auth/index', {
+   //       nombrePagina: 'Registro de Calificaciones',
+   //       pageAuthIndex: true,
+   //       layout: 'auth-layout',
+   //    });
+};
 
 exports.recuperacion = (req, res) => {
-    return res.render('auth/recuperacion',{
-        nombrePagina: 'Auth - Recuperacion',
-        pageAuthLogin: true,
-        layout: false
-    })
-}
+   return res.render('auth/recuperacion', {
+      nombrePagina: 'Auth - Recuperacion',
+      pageAuthLogin: true,
+      layout: false,
+   });
+};
 
 exports.newPassword = (req, res) => {
-    return res.render('auth/new_password',{
-        nombrePagina: 'Auth - New Password',
-        pageAuthLogin: true,
-        layout: false
-    })
-}
+   return res.render('auth/new_password', {
+      nombrePagina: 'Auth - New Password',
+      pageAuthLogin: true,
+      layout: false,
+   });
+};
 // exports.pantalla_inicio = (req, res) => {
-//     res.render('modulos/registro-calificaciones/home', {
-//         nombrePagina: 'Calificaciones'
-//     })
-// }
+//    res.render('modulos/registro-calificaciones/home', {
+//       nombrePagina: 'Calificaciones',
+//    });
+// };
--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-
 // controllers
-const authCtrl = require('../controllers/auth/AuthCtrl'); 
+const authCtrl = require('../controllers/auth/AuthCtrl');
 const calificacionesCtrl = require('../controllers/registro_calificaciones/ModRegistroCalificacionesCtrl');
-//const { check } = require('express-validator'); 
+//const { check } = require('express-validator');
 
 router.get('/auth/login', authCtrl.pageAuth);
 router.get('/auth/recuperacion', authCtrl.recuperacion);
 router.get('/auth/new_password', authCtrl.newPassword);
 router.get('/', authCtrl.pantalla_inicio);
 
+router.get('/home', authCtrl.home);
+
 router.get('/calificaciones', calificacionesCtrl.pantalla_inicio);
 // router.get('/calificaciones', require('./registro_calificaciones'));
 
-
-router.get('*', (req, res) => res.redirect('/') );
+router.get('*', (req, res) => res.redirect('/'));
 module.exports = router;
