713c40c72eeb61f627d06139d8f2bc49b99de0da
JeralPineda <jeral.cris@gmail.com>
2021-04-03 22:00:02 -0600

    middlleware para validar campos del login

--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -4,10 +4,13 @@ const router = express.Router();
 // controllers
 const authCtrl = require('../controllers/auth/AuthCtrl');
 const calificacionesCtrl = require('../controllers/registro_calificaciones/ModRegistroCalificacionesCtrl');
-//const { check } = require('express-validator');
+const { check } = require('express-validator');
 
-// const auth = require('../../api/modulo_login/routes/auth');
+// Controlador modulo login
 const { login } = require('../../api/modulo_login/controllers/AuthCtrl');
+
+// Middlewares modulo login
+const { validarCampos } = require('../../api/modulo_login/middlewares/validar-campos');
 
 router.get('/', authCtrl.pantalla_inicio);
 router.get('/auth/login', authCtrl.pageAuth);
@@ -21,7 +24,7 @@ router.get('/home', authCtrl.home);
 router.get('/calificaciones', calificacionesCtrl.pantalla_inicio);
 
 // Rutas API modulo_login
-router.post('/api/auth/login', login);
+router.post('/api/auth/login', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('password', 'La contraseña debe contener al menos 4 dijitos y es requerido').isLength({ min: 4 }), validarCampos], login);
 
 router.get('*', (req, res) => res.redirect('/'));
 module.exports = router;
