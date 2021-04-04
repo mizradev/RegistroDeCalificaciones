23236154c4f955ee5b6a2448a1ef15808893bfc4
JeralPineda <jeral.cris@gmail.com>
2021-04-04 11:17:58 -0600

    Autenticacion metodo bearer

--- src/api/modulo_login/middlewares/validar-jwt.js
+++ src/api/modulo_login/middlewares/validar-jwt.js
@@ -1,38 +1,44 @@
 const jwt = require('jsonwebtoken');
 const { request, response } = require('express');
+
 const user = require('../model/users');
 
 const validarJWT = async (req = request, res = response, next) => {
-	// leer los header desde postman
-	const token = req.header('x-token');
+	// leer los header desde header método (Bearer)
+	const xToken = req.get('x-token');
+	let token = '';
 
-	//    Si no viene el token
-	if (!token) {
-		return res.status(401).json({
-			msg: 'No hay token en la petición',
-		});
+	// si existe token y el Bearer desde el header viene en minúsculas
+	if (xToken && xToken.toLowerCase().startsWith('bearer')) {
+		//extraemos el parámetro bearer del token
+		token = xToken.substring(7);
 	}
 
-	//    validar jwt
+	if (!token) return res.status(401).json({ msg: 'No tiene autorización' });
+
 	try {
+		// // Obtenemos el uid del payload
 		const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
-		// obtengo uid del token y lo guardo en la request
+		// console.log(uid);
+
+		if (!uid) return res.status(401).json({ msg: 'El usuario no existe' });
+
+		// Guardamos el uid en los request
 		req.uid = uid;
-		console.log(uid);
 
-		// Leer el usuario que corresponde uid
+		// Obtenemos la información del usuario autenticado a partir del req.uid
 		const usuario = await user.getRol(req.uid);
-		if (!usuario) return res.status(401).json({ msg: 'El usuario no existe' });
 
+		// Guardamos la informacion del usuario en el request
 		req.usuario = usuario;
 
-		console.log(usuario);
+		// console.log(usuario);
 
 		next();
 	} catch (error) {
 		console.log(error);
 		res.status(401).json({
-			msg: 'Token no valido',
+			msg: 'No esta autenticado',
 		});
 	}
 };
