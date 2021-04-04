4bbdd81c5e5353bd4f1b9cddb9306af2386fa0f2
JeralPineda <jeral.cris@gmail.com>
2021-04-03 21:59:29 -0600

    Validacion y generar token al logearse

--- src/api/modulo_login/controllers/AuthCtrl.js
+++ src/api/modulo_login/controllers/AuthCtrl.js
@@ -1,91 +1,45 @@
-const mysqlConnection = require('../../../config/db');
 const bcryptjs = require('bcryptjs');
 
+const { getEmail } = require('../model/users');
+const { generarJWT } = require('../helpers/generar-jwt');
+
 const login = async (req, res) => {
-   const email = req.body.correo;
-   const password = req.body.password;
+	//    encriptar
+	//    const salt = bcryptjs.genSaltSync();
+	//    const password1 = bcryptjs.hashSync(password, salt);
+	//    console.log(password1);
 
-   //    encriptar
-   //    const salt = bcryptjs.genSaltSync();
-   //    const password1 = bcryptjs.hashSync(password, salt);
-   //    console.log(password1);
+	try {
+		// Modelo de datos de usuario
+		const usuario = await getEmail(req.body.correo);
 
-   //validación de los campos
-   let expReg = /[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i.test(email);
+		// Verificar si el correo existe
+		if (usuario === undefined) {
+			return res.status(400).json({ error: 'El usuario o la contraseña son invalidos - correo' });
+		}
 
-   let pass = /^.{4,12}$/.test(password);
+		// Si el usuario esta activo
+		if (usuario.indicador_usuario !== 'activo') {
+			return res.status(400).json({ error: 'El usuario o la contraseña son invalidos - false' });
+		}
 
-   if (!expReg) return res.status(400).json({ msg: 'Debes ingresar un correo valido' });
+		// Verificar la contraseña
+		const validarPassword = bcryptjs.compareSync(req.body.password, usuario.password);
+		if (!validarPassword) {
+			return res.status(400).json({ error: 'El usuario o la contraseña son invalidos - password' });
+		}
 
-   //    Validar la contraseña
-   if (!pass) return res.status(400).json({ msg: 'Tu contraseña debe contener al menos 4 dígitos' });
+		// Generar el token
+		const token = await generarJWT(usuario.id_usuario);
 
-   // Validar que ambos campos no estén vacíos
-   if (!(email && password)) {
-      return res.status(400).json({ message: 'Se requiere el correo y la contraseña' });
-   }
-
-   try {
-      //Consulta para obtener la contraseña encriptada
-      mysqlConnection.query(`SELECT password FROM usuarios WHERE correo = ?`, [email], (err, pass) => {
-         if (err) {
-            return res.status(500).json({
-               ok: false,
-               msg: error,
-            });
-         }
-
-         if (pass.length > 0) {
-            //  obtengo la contraseña desde la base de datos
-            const passw = pass[0].password;
-
-            // Validar contraseña
-            const validarPasswor = bcryptjs.compareSync(password, passw);
-            if (!validarPasswor) return res.status(400).json({ msg: 'El correo o la contraseña son inválidos' });
-
-            //  Segunda consulta para el login
-            mysqlConnection.query(`SELECT * FROM usuarios WHERE correo = ? and password= ? `, [email, passw], (error, results) => {
-               if (error) {
-                  return res.status(500).json({
-                     ok: false,
-                     msg: error,
-                  });
-               }
-
-               if (results.length > 0) {
-                  // Consulta para ver si el usuario esta activo
-                  mysqlConnection.query(`SELECT indicador_usuario, id_rol FROM usuarios WHERE correo = ? `, [email], (error, result) => {
-                     if (error) {
-                        return res.status(500).json({
-                           ok: false,
-                           msg: error,
-                        });
-                     } else {
-                        // Si el usuario esta activo
-                        if ('Activo' === result[0].indicador_usuario && result[0].id_rol === 9) {
-                           res.status(200).json(results);
-                        } else {
-                           res.status(400).json({ message: 'Este usuario no existe' });
-                        }
-                     }
-                  });
-               } else {
-                  res.status(400).json({ message: 'El correo o la contraseña son inválidos' });
-               }
-            });
-            // Fin consulta login
-         } else {
-            res.status(400).json({ message: 'El correo o la contraseña son inválidos' });
-         }
-      });
-      //   Fin consulta validar contraseña
-   } catch (error) {
-      console.log(error);
-      return res.status(500).json({ msg: 'Hable con el administrador' });
-   }
-   //    Fin try-catch
+		res.status(200).json({ usuario, message: 'Login ok', token: token });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: 'Hable con el administrador' });
+	}
+	//    Fin try-catch
 };
 
 module.exports = {
-   login,
+	login,
 };
