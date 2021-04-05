18fd00a2ea67a2b2e3c56593d4fa96c036d06e92
JeralPineda <jeral.cris@gmail.com>
2021-04-04 23:00:53 -0600

    Validar jwt y el Rol

--- src/api/modulo_login/middlewares/validar-jwt.js
+++ src/api/modulo_login/middlewares/validar-jwt.js
@@ -1,48 +1,27 @@
 const jwt = require('jsonwebtoken');
-const { request, response } = require('express');
 
-const user = require('../model/users');
+const authToken = function (req, res, next) {
+	if (req.path != '/api/auth/login') {
+		if (req.headers.authorization) {
+			let token = req.headers.authorization.split(' ')[1];
 
-const validarJWT = async (req = request, res = response, next) => {
-	// leer los header desde header método (Bearer)
-	const xToken = req.get('x-token');
-	let token = '';
+			jwt.verify(token, process.env.SECRETORPRIVATEKEY, function (error, decoded) {
+				if (error) return res.status(401).send({ message: 'No tienes los permisos suficientes para estar aquí...', error });
 
-	// si existe token y el Bearer desde el header viene en minúsculas
-	if (xToken && xToken.toLowerCase().startsWith('bearer')) {
-		//extraemos el parámetro bearer del token
-		token = xToken.substring(7);
-	}
-
-	if (!token) return res.status(401).json({ msg: 'No tiene autorización' });
-
-	try {
-		// // Obtenemos el uid del payload
-		const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
-		// console.log(uid);
-
-		if (!uid) return res.status(401).json({ msg: 'El usuario no existe' });
-
-		// Guardamos el uid en los request
-		req.uid = uid;
-
-		// Obtenemos la información del usuario autenticado a partir del req.uid
-		const usuario = await user.getRol(req.uid);
-
-		// Guardamos la informacion del usuario en el request
-		req.usuario = usuario;
-
-		// console.log(usuario);
-
-		next();
-	} catch (error) {
-		console.log(error);
-		res.status(401).json({
-			msg: 'No esta autenticado',
-		});
-	}
+				if (decoded.estado !== 'activo') return res.status(401).send({ message: 'No existe este usuario', error });
+				next();
+				if (req.method != 'GET') {
+					// console.log(decode.rol);
+					if (decoded.rol === 9) next();
+					else res.status(401).send({ message: 'No tienes los permisos suficientes para estar aquí... rol' });
+				} else {
+					next();
+				}
+			});
+		} else res.status(401).send({ message: 'No tienes los permisos suficientes para estar aquí...' });
+	} else next();
 };
 
 module.exports = {
-	validarJWT,
+	authToken,
 };
