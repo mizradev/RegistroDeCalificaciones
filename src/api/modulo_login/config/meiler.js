360566ded27bb0e2d11938dc564bad5dd0371755
JeralPineda <jeral.cris@gmail.com>
2021-04-06 01:34:51 -0600

    transporter de las credenciales envio de email

---
+++ src/api/modulo_login/config/meiler.js
@@ -0,0 +1,14 @@
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+	host: process.env.MEILERHOST,
+	port: process.env.MEILERPORT,
+	secure: false,
+	auth: {
+		user: process.env.MEILERUSERNAME,
+		pass: process.env.MEILERPASSWORD,
+	},
+});
+
+module.exports = transporter;
