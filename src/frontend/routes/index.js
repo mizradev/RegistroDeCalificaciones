ae8b153cd9e5769df9243f04f6263741338e127a
JeralPineda <jeral.cris@gmail.com>
2021-04-05 19:34:52 -0600

    Rutas new y update password

--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -7,7 +7,7 @@ const calificacionesCtrl = require('../controllers/registro_calificaciones/ModRe
 const { check } = require('express-validator');
 
 // Controlador modulo login
-const { login } = require('../../api/modulo_login/controllers/AuthCtrl');
+const { login, recuperarPassword, newPassword } = require('../../api/modulo_login/controllers/AuthCtrl');
 const { user, userPost } = require('../../api/modulo_login/controllers/userCtrl');
 
 // Middlewares modulo login
@@ -25,14 +25,21 @@ router.get('/home', authCtrl.home);
 
 router.get('/calificaciones', calificacionesCtrl.pantalla_inicio);
 
+// Middleware para validar si tiene token, si esta activo y el rol
 router.use(authToken);
 
 // Rutas API modulo_login
-router.post('/api/auth/login', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('password', 'La contraseña debe contener al menos 4 dijitos y es requerido').isLength({ min: 4 }), validarCampos], login);
+router.post('/api/auth/login', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], login);
+
+// Ruta recuperar contraseña
+router.post('/api/auth/recuperarContrasenia', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), validarCampos], recuperarPassword);
+
+// Ruta para crear la nueva contraseña
+router.post('/api/auth/nuevaContrasenia', [check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], newPassword);
 
 router.get('/api/usuarios/:id', user);
 
-router.post('/api/usuarios', [check('correo', 'El correo es obligatorio').isEmail(), check('password', 'La contraseña debe contener al menos 4 dijitos y es requerido').isLength({ min: 4 }), validarCampos], userPost);
+router.post('/api/usuarios', [check('correo', 'El correo es obligatorio').isEmail(), check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], userPost);
 
 router.get('*', (req, res) => res.redirect('/'));
 module.exports = router;
