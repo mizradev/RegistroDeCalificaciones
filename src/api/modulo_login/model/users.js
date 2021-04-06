07b05a8add2a702f6ea51c280a66f33d173e13fe
JeralPineda <jeral.cris@gmail.com>
2021-04-05 19:30:07 -0600

    nuevas consulatas para recuperar contrasenia

--- src/api/modulo_login/model/users.js
+++ src/api/modulo_login/model/users.js
@@ -45,9 +45,9 @@ const getUsuario = (id) => {
 };
 
 // crear usuario prueba
-const postUsuario = (id_usuario, id_rol, user, password, correo, token_password, token_expiracion, indicador_usuario, usr_registro, fecha_registro) => {
+const postUsuario = (id_usuario, id_rol, user, password, correo, token_password, token_expiracion, indicador_usuario, usr_registro) => {
 	return new Promise((resolve, reject) => {
-		mysqlConnection.query(`INSERT INTO usuarios (id_usuario, id_rol, user, password, correo, token_password, token_expiracion, indicador_usuario, usr_registro, fecha_registro) VALUES (?, ?, ?, ?, ?, ?)`, [id_usuario, id_rol, user, password, correo, token_password, token_expiracion, indicador_usuario, usr_registro, fecha_registro], (err, usuario) => {
+		mysqlConnection.query(`INSERT INTO usuarios (id_usuario, id_rol, user, password, correo, token_password, token_expiracion, indicador_usuario, usr_registro) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, [id_usuario, id_rol, user, password, correo, token_password, token_expiracion, indicador_usuario, usr_registro], (err, usuario) => {
 			if (err) {
 				reject(err);
 				console.log(err);
@@ -58,9 +58,54 @@ const postUsuario = (id_usuario, id_rol, user, password, correo, token_password,
 	});
 };
 
+// Insertar token_password a usuarios por email
+const postToken = (token, id) => {
+	return new Promise((resolve, reject) => {
+		mysqlConnection.query('UPDATE usuarios SET token_password = ? WHERE usuarios.id_usuario = ?', [token, id], (err, row) => {
+			if (err) {
+				reject(err);
+				console.log(err);
+			} else {
+				resolve(row[0]);
+			}
+		});
+	});
+};
+
+// Cambiar contraseña y limpiar el campo del token
+const postPassword = (id, password) => {
+	return new Promise((resolve, reject) => {
+		mysqlConnection.query('UPDATE usuarios SET password = ? WHERE usuarios.id_usuario = ?', [id, password], (err, result) => {
+			if (err) {
+				reject(err);
+				console.log(err);
+			} else {
+				resolve(result[0]);
+			}
+		});
+	});
+};
+
+// Limpiar el campo token_password después de cambiar la contraseña
+const limpiarToken = (id, token_password) => {
+	return new Promise((resolve, reject) => {
+		mysqlConnection.query('UPDATE usuarios SET token_password = ? WHERE usuarios.id_usuario = ?', [id, token_password], (err, rows) => {
+			if (err) {
+				reject(err);
+				console.log(err);
+			} else {
+				resolve(rows[0]);
+			}
+		});
+	});
+};
+
 module.exports = {
 	getEmail,
 	getRol,
 	getUsuario,
 	postUsuario,
+	postToken,
+	postPassword,
+	limpiarToken,
 };
