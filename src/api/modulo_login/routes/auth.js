2da1348fe4c041176c8cb20bc982f37e9803dbed
JeralPineda <jeral.cris@gmail.com>
2021-04-11 18:13:40 -0600

    eliminando routes sin uso

--- src/api/modulo_login/routes/auth.js
+++
@@ -1,9 +0,0 @@
-// // const router = require('express').Router();
-// const express = require('express');
-// // const mysqlConnection = require('../../../config/db');
-// const { login } = require('../controllers/AuthCtrl');
-// const routes = express.Router();
-
-// routes.get('/login', login);
-
-// module.exports = routes;
