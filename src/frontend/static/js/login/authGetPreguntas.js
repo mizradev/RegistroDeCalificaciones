51dd06ed4dbb52b6812eadda5bf848b5b5ce2a2e
jeral <jeral.cris@gmail.com>
2021-04-23 17:03:09 -0600

    peticion para mostrar preguntas en el select

---
+++ src/frontend/static/js/login/authGetPreguntas.js
@@ -0,0 +1,28 @@
+(() => {
+   let userData = [];
+   const pregunta = $('#formPreguntas #pregunta');
+
+   const getPreguntas = async () => {
+      try {
+         const res = await axios.get('/api/preguntas');
+
+         // guradamos los datos
+         userData = res.data;
+
+         //  Creamos html
+         pregunta.append(` <option value="${userData.id_preguntas}">${userData.pregunta_1}</option>`);
+         pregunta.append(` <option value="${userData.id_preguntas}">${userData.pregunta_2}</option>`);
+      } catch (error) {
+         console.log(error);
+         // Accedemos al message de la data del error y lo guardamos
+         const message = error.response.data.message;
+
+         Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: message,
+         });
+      }
+   };
+   getPreguntas();
+})();
