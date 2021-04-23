6677238b801151e45e7c7df77c8dde829e313ebe
jeral <jeral.cris@gmail.com>
2021-04-22 18:15:13 -0600

    Configurando las rutas

--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -7,7 +7,7 @@ const authCtrl = require('../controllers/auth/AuthCtrl');
 const calificacionesCtrl = require('../controllers/registro_calificaciones/ModRegistroCalificacionesCtrl');
 
 // Controladores modulo login
-const { login, recuperarPassword, newPassword, passwordPreguntas } = require('../../api/modulo_login/controllers/AuthCtrl');
+const { login, recuperarPassword, newPassword2, passwordPreguntas } = require('../../api/modulo_login/controllers/AuthCtrl');
 const { user } = require('../../api/modulo_login/controllers/userCtrl');
 const { preguntas, getAllPreguntas } = require('../../api/modulo_login/controllers/preguntasCtrl');
 
@@ -31,13 +31,13 @@ router.get('/calificaciones', calificacionesCtrl.pantalla_inicio);
 router.post('/api/auth/login', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], login);
 
 // Ruta recuperar contraseña
-router.get('/api/auth/recuperarContrasenia', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), validarCampos], recuperarPassword);
+router.post('/api/auth/recuperarContrasenia', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), validarCampos], recuperarPassword);
 
 // Ruta recuperar contraseña por preguntas
-router.get('/api/auth/recuperarPorPreguntas', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('respuesta', 'La respuesta es obligatoria').not().isEmpty(), validarCampos], passwordPreguntas);
+router.post('/api/auth/recuperarPorPreguntas', [check('correo', 'Debe ser un correo valido y es requerido').isEmail(), check('respuesta', 'La respuesta es obligatoria').not().isEmpty(), validarCampos], passwordPreguntas);
 
 // Ruta para crear la nueva contraseña
-router.put('/api/auth/nuevaContrasenia/:token', [check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], newPassword);
+router.put('/api/auth/nuevaContrasenia', [check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], newPassword2);
 // router.put('/api/auth/nuevaContrasenia', [check('password', 'La contraseña debe contener al menos 4 dígitos y es requerido').isLength({ min: 4 }), validarCampos], newPassword);
 
 // Ruta para probar los middlewares
