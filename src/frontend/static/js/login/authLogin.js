93bc95a5757205fbe3595b581339ab65c3e819c4
jeral <jeral.cris@gmail.com>
2021-04-24 13:37:34 -0600

    protegiendo la ruta login

--- src/frontend/static/js/login/authLogin.js
+++ src/frontend/static/js/login/authLogin.js
@@ -1,62 +1,70 @@
 (() => {
-   $('#formLogin').on('submit', async (e) => {
-      e.preventDefault();
+   // Extraemos el token del local estorage para comprobar que existe
+   const validToken = localStorage.getItem('token');
+   if (!validToken) {
+      $('#formLogin').on('submit', async (e) => {
+         e.preventDefault();
 
-      const correo = $('#correo').val();
-      const password = $('#password').val();
+         const correo = $('#correo').val();
+         const password = $('#password').val();
 
-      if (correo.length >= 4 && password.length >= 4) {
-         datos = {
-            correo,
-            password,
-         };
+         if (correo.length >= 4 && password.length >= 4) {
+            datos = {
+               correo,
+               password,
+            };
 
-         try {
-            const res = (await axios.post('/api/auth/login', datos)).data;
+            try {
+               const res = (await axios.post('/api/auth/login', datos)).data;
 
-            console.log(res);
+               console.log(res);
 
-            // Extraemos el token y lo guardamos en localstorage
-            const { token } = res;
-            localStorage.setItem('token', token);
+               // Extraemos el token y lo guardamos en localstorage
+               const { token } = res;
+               localStorage.setItem('token', token);
 
-            Swal.fire({
-               icon: 'success',
-               text: res.message,
-            }).then((result) => {
-               if (result.isConfirmed) {
+               Swal.fire({
+                  icon: 'success',
+                  text: res.message,
+                  showConfirmButton: false,
+                  timer: 1500,
+               }).then(() => {
                   // limpiamos el formulario
                   $('#formLogin')[0].reset();
 
                   // Redireccionar
                   const url = '/home';
                   $(location).attr('href', url);
-               }
-            });
-         } catch (error) {
-            console.log(error.response.data);
-            // Accedemos al message de la data del error y lo guardamos
-            const message = error.response.data.message;
+               });
+            } catch (error) {
+               console.log(error.response.data);
+               // Accedemos al message de la data del error y lo guardamos
+               const message = error.response.data.message;
 
+               Swal.fire({
+                  icon: 'error',
+                  title: 'Error',
+                  text: message,
+                  showConfirmButton: false,
+                  timer: 1500,
+               }).then(() => {
+                  // limpiamos el formulario
+                  $('#formLogin')[0].reset();
+               });
+            }
+         } else {
+            //   Si el usuario i ngresa mal un campo
             Swal.fire({
                icon: 'error',
-               title: 'Error',
-               text: message,
-            }).then((result) => {
-               // limpiamos el formulario
-               if (result.isConfirmed) {
-                  $('#formLogin')[0].reset();
-               }
+               title: 'Debes completar los campos correctamente',
+               showConfirmButton: false,
+               timer: 1500,
             });
          }
-      } else {
-         //   Si el usuario i ngresa mal un campo
-         Swal.fire({
-            icon: 'error',
-            title: 'Debes completar los campos correctamente',
-            showConfirmButton: false,
-            timer: 1500,
-         });
-      }
-   });
+      });
+   } else {
+      //   Redireccionamos si no existe el token
+      const url = '/home';
+      $(location).attr('href', url);
+   }
 })();
