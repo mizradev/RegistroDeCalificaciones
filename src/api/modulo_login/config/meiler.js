b20b07e97d19ee479c605d6a1c8d7dd7205c9df4
Jeral <jeral.cris@gmail.com>
2021-05-26 14:47:18 -0600

    Permitiendo seguridad en el correo

--- src/api/modulo_login/config/meiler.js
+++ src/api/modulo_login/config/meiler.js
@@ -2,13 +2,13 @@ const nodemailer = require('nodemailer');
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-	host: process.env.MEILERHOST,
-	port: process.env.MEILERPORT,
-	secure: false,
-	auth: {
-		user: process.env.MEILERUSERNAME,
-		pass: process.env.MEILERPASSWORD,
-	},
+   host: process.env.MEILERHOST,
+   port: process.env.MEILERPORT,
+   secure: true,
+   auth: {
+      user: process.env.MEILERUSERNAME,
+      pass: process.env.MEILERPASSWORD,
+   },
 });
 
 module.exports = transporter;
