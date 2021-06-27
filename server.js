147b7d77b3eeb1f923352341559a8c30ea0dfefc
TATO06 <82806180+TATO06@users.noreply.github.com>
2021-06-27 15:29:27 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const app = express();
 
-const calificacionesCtrl = require('./src/frontend/controllers/registro_calificaciones/ModRegistroCalificacionesCtrl')
+
 // enviroments vars
 require('dotenv').config();
 
@@ -14,20 +14,32 @@ app.engine('.hbs',
         extname: '.hbs'
     })
 );
-app.use(express.static(path.join(__dirname+'/src/frontend', 'static')));
 app.set('view engine', '.hbs');
 app.set('views', './src/frontend/views');
 
+app.use(express.static(path.join(__dirname+'/src/frontend', 'static')));
+
 //Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3002);
 
 //Middlewares
+
+
 app.use(express.json({extended: true}));
 
+
+
 //Routes pages
+//app.use(require('./src/api/routes/index')); llamado al archivo subir img 
 app.use('/', require('./src/frontend/routes'));
+
 // Route api
 app.use('/api/v1/calificaciones', require('./src/api/registro_calificaciones/routes'));
+app.use('/api/v1/mensaje', require('./src/api/modulo_ticket/routes'));
+
+app.use('/api/v1/estudiante', require('./src/api/estudiante/routes'));
+
+
 
 //Starting the server
 app.listen(app.get('port'), () => console.log(`server on port http://localhost:${app.get('port')}`));
