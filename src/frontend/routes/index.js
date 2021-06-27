f70fccc2464229ab32ba7fc424e5d0c56e217ed1
soymizra <jorgemizrain@gmail.com>
2021-06-27 00:12:32 -0600

    Conflicto resuelto

--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 // controllers vistas
 const authCtrl = require('../controllers/auth/AuthCtrl');
+const calificacionesCtrl = require('../controllers/registro_calificaciones/ModRegistroCalificacionesCtrl');
+//Modulo Usuario
+const UserCtrl = require('../controllers/mod_Usuario/modUsuarioCtrl'); 
 
 // rutas de las pantallas modulo_login
 router.get('/', authCtrl.pantalla_inicio);
@@ -15,18 +18,6 @@ router.get('/auth/preguntas', authCtrl.preguntas);
 router.get('/auth/respuestas/:token', authCtrl.respuestas);
 router.get('/home', authCtrl.home);
 
-// router.get('*', (req, res) => res.redirect('/'));
-module.exports = router;
-
-// controllers
-const authCtrl = require('../controllers/auth/AuthCtrl'); 
-const calificacionesCtrl = require('../controllers/registro_calificaciones/ModRegistroCalificacionesCtrl');
-//Modulo Usuario
-
-const UserCtrl = require('../controllers/mod_Usuario/modUsuarioCtrl'); 
-//const validaciones = requiere('../static/js/Usuario/validaciones'); //Validaciones
-//const { check } = require('express-validator'); 
-router.get('/', authCtrl.pantalla_inicio);
 
 router.get('/calificaciones', calificacionesCtrl.pantalla_inicio);
 //router.get('/usuario',UsuarioCtrl.pantalla_administrador);
